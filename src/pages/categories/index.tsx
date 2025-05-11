bb7587ef077133ee602466fe0c87836c359c4dc3
amin <aminmansuri77z99@gmail.com>
2025-05-11 12:19:07 +0330

    force-dynamic on myaddresses

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -4,7 +4,7 @@ import Loading from "../../components/ui/Loading";
 import React, { useEffect, useState } from "react";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { useSession } from "next-auth/react";
-import dynamic from "next/dynamic";
+import Dynamic from "next/dynamic";
 import { useRecoilState } from "recoil";
 import { buyerLocationAtom } from "../../../store/atoms/buyerLocationAtom";
 import Header from "../../components/Header";
@@ -13,7 +13,7 @@ import ProductImg from "../../../public/images/p2.webp";
 import { MdOutlineDeleteForever } from "react-icons/md";
 
 import toast from "react-hot-toast";
-const Map = dynamic(() => import("../../components/MyMap"), {
+const Map = Dynamic(() => import("../../components/MyMap"), {
   ssr: false,
 });
 
@@ -352,6 +352,7 @@ const CategoryProductsPage = () => {
     </div>
   );
 };
+export const dynamic = "force-dynamic";
 
 export default CategoryProductsPage;
 // export async function getServerSideProps() {
