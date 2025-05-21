0d0721d0a9ab0756e5dba7923e87d0b72f36db87
amin <aminmansuri77z99@gmail.com>
2025-05-21 18:12:34 +0330

    hardcode the api keys on debug api

--- src/pages/api/debug.ts
+++ src/pages/api/debug.ts
@@ -3,8 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 export default async function handler(req, res) {
   try {
     const supabase = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+      "https://ybkqlbrfanevwwmeykpr.supabase.co",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlia3FsYnJmYW5ldnd3bWV5a3ByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczNTc1NDIsImV4cCI6MjA1MjkzMzU0Mn0.It8KICwnXtW9Ekw6mCvt6m45fPBG0D2paqUdIyn0wJ4"!
     );
 
     const { data, error } = await supabase
